75163b4d8eb7e21f5e2b9466b7336470e405921b
Shreyash Mehrotra <shreyashmehrotra20@gmail.com>
2023-08-29 19:33:05 +0530

    Photos update board

--- src/components/Board/static/boardDetails.js
+++ src/components/Board/static/boardDetails.js
@@ -1,74 +1,82 @@
 import Afifa from './Afifa.jpeg';
 import Bhavana from './Bhavana.jpg';
-import Harshit from './Harshit.jpeg';
 import Niranjan from './Niranjan.jpg';
 import Pavani from './Pavani.jpeg';
 import Siddharth from './Siddharth.jpeg';
 import Vineesh from './Vineesh.jpg';
-import Tarun from './Tarun.jpeg';
 import Ujwala from './Ujwala.jpeg';
 import Yashwant from './Yashwant.jpeg';
+import Shreyash from './shreyash.png';
+import Priyansh from './priyansh.jpg';
+import Sanyam from './sanyam.jpg';
+import Tanvir from './tanvir.png';
+import Jagruti from './jagruti.png';
+import Akash from './akash.png';
+import Vinyas from './vinyas.png';
+import Varun from './varun.png';
+import Ritocheta from './ritocehta.png';
+import Nikhil from './nikhil.png';
 
 export const boardList = [
     {
         designation: "Chairperson",
         name: "Tanvir Mungekar",
         linkedInProfile: "https://www.linkedin.com/in/tanvir-mungekar-491aa8202/",
-        imageSrc: Yashwant
+        imageSrc: Tanvir
     },
     {
         designation: "Co-Chairperson",
         name: "Jagruti Mahapatra",
         linkedInProfile: "https://www.linkedin.com/in/jagruti02/",
-        imageSrc: Vineesh
+        imageSrc: Jagruti
     },
     {
         designation: "Secretary",
         name: "Sanyam Jain",
         linkedInProfile: "https://www.linkedin.com/in/sanyam-jain-611577237/",
-        imageSrc: Bhavana
+        imageSrc: Sanyam
     },
     {
         designation: "Co-Secretary",
         name: "Priyansh Jain",
         linkedInProfile: "https://www.linkedin.com/in/priyansh-jain-6455891b8/",
-        imageSrc: Tarun
+        imageSrc: Priyansh
     },
     {
         designation: "Tech Chair",
         name: "Shreyash Mehrotra",
         linkedInProfile: "https://www.linkedin.com/in/shreyash-mehrotra-637a50201/",
-        imageSrc: Harshit
+        imageSrc: Shreyash
     },
     {
         designation: "Events Chair",
         name: "Akash Mishra",
         linkedInProfile: "https://www.linkedin.com/in/akashmishraa/",
-        imageSrc: Pavani
+        imageSrc: Akash
     },
     {
         designation: "Publicity Chair",
         name: "Vinyas Shetty",
         linkedInProfile: "https://www.linkedin.com/in/vinyasshetty/",
-        imageSrc: Ujwala
+        imageSrc: Vinyas
     },
     {
         designation: "Creative Chair",
         name: "Varun Jindal",
         linkedInProfile: "https://www.linkedin.com/in/varun-jindal-767a19202/",
-        imageSrc: Siddharth
+        imageSrc: Varun
     },
     {
         designation: "Editorial Chair",
         name: "Ritocheta Roy",
         linkedInProfile: "https://www.linkedin.com/in/ritocheta-roy-bb5803218/",
-        imageSrc: Afifa
+        imageSrc: Ritocheta
     },
     {
         designation: "Finance Chair",
         name: "Nikhi Dangi",
         linkedInProfile: "https://www.linkedin.com/in/nikhil-dangi-835124201/",
-        imageSrc: Niranjan
+        imageSrc: Nikhil
     },
 
 ];
